548fb060cd55101997f740e854523e67c35dcb86
Inna <innainnaivanov@yandex.ru>
2025-11-27 15:05:27 +0300

    Add modal window using React Portal, add theme selection using React Context,, add key and React Fragment for lists

--- homework-1/src/widgets/LayoutHeader/Header.tsx
+++ homework-1/src/widgets/LayoutHeader/Header.tsx
@@ -1,9 +1,29 @@
 import styles from './Header.module.css';
+import ThemeSwitcher from '../../features/ThemeSwitcher/ui/ThemeSwitcher';
+import { useTheme } from '../../shared/lib/theme/useTheme';
+import Button from '../../shared/ui/Button';
+import Modal from '../../shared/ui/Modal';
+import React, { useState } from 'react';
+
 
 function Header() {
+  const { theme } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className={styles.header}>
-      <h1>Мой блог</h1>
+    <div className={`${styles.header} ${theme}`}>
+      <div className={styles.left}>
+        <h1>My blog</h1>
+      </div>
+
+      <div className={styles.right}>
+        <ThemeSwitcher />
+        <Button onClick={() => setIsOpen(true)}>О проекте</Button>
+      </div>
+
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <p>Это учебный проект для практики  React.</p>
+      </Modal>
     </div>
   );
 }
